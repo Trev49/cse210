3dbf2cc3319afe550bade5c26184a68a52f65a62
Trev49 <trevor.debernardi@gmail.com>
2025-01-09 17:12:19 -0700

    Name program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hi There! Great to see you!");
+        Console.WriteLine("What's your name?");
+        string First_name = Console.ReadLine();
+        Console.WriteLine("Now, what's your last name?");
+        string Last_name = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you {First_name} {Last_name}!");
+
+
+
     }
 }
